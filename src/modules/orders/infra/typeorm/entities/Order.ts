74bbc4a6ad3076573a89a4f0936b39d8398a0c35
Allysson C. da Silva <allysson.cheguera@hotmail.com>
2020-07-01 18:23:46 -0300

    :sparkles: challenge finalized

--- src/modules/orders/infra/typeorm/entities/Order.ts
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -4,22 +4,36 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   ManyToOne,
-  JoinColumn,
   OneToMany,
+  Column,
+  JoinColumn,
 } from 'typeorm';
 
 import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('orders')
 class Order {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
+  customer_id: string;
+
+  @ManyToOne(() => Customer, { eager: true })
+  @JoinColumn({ name: 'customer_id' })
   customer: Customer;
 
+  @OneToMany(() => OrdersProducts, order_products => order_products.order, {
+    cascade: true,
+    eager: true,
+  })
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,59 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customerExists = await this.customersRepository.findById(customer_id);
+
+    if (!customerExists) {
+      throw new AppError('Customer not exists');
+    }
+
+    const productIds = products.map(product => ({ id: product.id }));
+
+    const productsReturn = await this.productsRepository.findAllById(
+      productIds,
+    );
+
+    if (productsReturn.length !== products.length) {
+      throw new AppError('Product not exists');
+    }
+
+    const productsFiltered = productsReturn.map((item, index) => {
+      return {
+        product_id: products[index].id,
+        quantity: products[index].quantity,
+        price: item.price,
+      };
+    });
+
+    const updatedProducts = productsReturn.map((item, index) => {
+      if (productsFiltered[index].quantity > item.quantity) {
+        throw new AppError('Stock not available for some products');
+      }
+
+      return Object.assign(item, {
+        quantity: item.quantity - productsFiltered[index].quantity,
+      });
+    });
+
+    await this.productsRepository.updateQuantity(updatedProducts);
+
+    const order = await this.ordersRepository.create({
+      customer: customerExists,
+      products: productsFiltered,
+    });
+
+    delete order.customer_id;
+
+    return order;
   }
 }
 
